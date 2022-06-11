973d604805e6fd51c38008dc4930302c723cc8e5
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-12 04:18:33 +0800

    refactor(lspsaga): use safe require instead

--- lua/plugins/lsp/handler.lua
+++ lua/plugins/lsp/handler.lua
@@ -25,67 +25,63 @@ end
 local use_custom_lsp_saga_config = false
 
 local function setup_lspaga()
-	local ok, lspsaga = pcall(require, "lspsaga")
+	SafeRequire('lspsaga', function(lspsaga)
+		local config = {}
 
-	if not ok then
-		return
-	end
+		if use_custom_lsp_saga_config then
+			config = {
+				-- defaults ...
+				debug = false,
+				use_saga_diagnostic_sign = true,
+				-- diagnostic sign
+				error_sign = "?",
+				warn_sign = "?",
+				hint_sign = "?",
+				infor_sign = "?",
+				diagnostic_header_icon = " ?  ",
+				-- code action title icon
+				code_action_icon = "? ",
+				code_action_prompt = {
+					enable = true,
+					sign = true,
+					sign_priority = 40,
+					virtual_text = true,
+				},
+				finder_definition_icon = "?  ",
+				finder_reference_icon = "?  ",
+				max_preview_lines = 10,
+				finder_action_keys = {
+					open = "o",
+					vsplit = "s",
+					split = "i",
+					quit = "q",
+					scroll_down = "<C-f>",
+					scroll_up = "<C-b>",
+				},
+				code_action_keys = {
+					quit = "q",
+					exec = "<CR>",
+				},
+				rename_action_keys = {
+					quit = "<C-c>",
+					exec = "<CR>",
+				},
+				definition_preview_icon = "?  ",
+				border_style = "single",
+				rename_prompt_prefix = "?",
+				rename_output_qflist = {
+					enable = false,
+					auto_open_qflist = false,
+				},
+				server_filetype_map = {},
+				diagnostic_prefix_format = "%d. ",
+				diagnostic_message_format = "%m %c",
+				highlight_prefix = false,
+			}
+		end
 
-	local config = {}
-
-	if use_custom_lsp_saga_config then
-		config = {
-			-- defaults ...
-			debug = false,
-			use_saga_diagnostic_sign = true,
-			-- diagnostic sign
-			error_sign = "?",
-			warn_sign = "?",
-			hint_sign = "?",
-			infor_sign = "?",
-			diagnostic_header_icon = " ?  ",
-			-- code action title icon
-			code_action_icon = "? ",
-			code_action_prompt = {
-				enable = true,
-				sign = true,
-				sign_priority = 40,
-				virtual_text = true,
-			},
-			finder_definition_icon = "?  ",
-			finder_reference_icon = "?  ",
-			max_preview_lines = 10,
-			finder_action_keys = {
-				open = "o",
-				vsplit = "s",
-				split = "i",
-				quit = "q",
-				scroll_down = "<C-f>",
-				scroll_up = "<C-b>",
-			},
-			code_action_keys = {
-				quit = "q",
-				exec = "<CR>",
-			},
-			rename_action_keys = {
-				quit = "<C-c>",
-				exec = "<CR>",
-			},
-			definition_preview_icon = "?  ",
-			border_style = "single",
-			rename_prompt_prefix = "?",
-			rename_output_qflist = {
-				enable = false,
-				auto_open_qflist = false,
-			},
-			server_filetype_map = {},
-			diagnostic_prefix_format = "%d. ",
-			diagnostic_message_format = "%m %c",
-			highlight_prefix = false,
-		}
-	end
-
-	lspsaga.setup(config)
+		lspsaga.setup(config)
+	end)
 end
 
 local function lsp_keymaps(bufnr)
