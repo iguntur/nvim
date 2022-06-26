1f7902687a48ad076ad70dcbe554b62c4d9ba53f
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-26 11:06:07 +0800

    chore(git): able jump into hunks

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -9,16 +9,44 @@ M.setup = function(use)
 		requires = {
 			"nvim-lua/plenary.nvim",
 		},
-		config = function()
-			local ok, gitsigns = pcall(require, "gitsigns")
-
-			if not ok then
-				return
-			end
-
-			gitsigns.setup()
-		end,
 	})
+
+	SafeRequire("gitsigns", function(gitsigns)
+		gitsigns.setup({
+			-- attach_to_untracked = true,
+			-- current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			-- current_line_blame_opts = {
+			-- 	virt_text = true,
+			-- 	virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+			-- 	delay = 1000,
+			-- 	ignore_whitespace = false,
+			-- },
+			-- current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map('n', ']c', function()
+					if vim.wo.diff then return ']c' end
+					vim.schedule(function() gs.next_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+
+				map('n', '[c', function()
+					if vim.wo.diff then return '[c' end
+					vim.schedule(function() gs.prev_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+			end,
+		})
+	end)
 end
 
 return M
