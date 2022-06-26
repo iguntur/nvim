082439aa57de76d772e76f4b5de63f80570b8141
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-26 08:11:02 +0800

    chore(treesitter): fix and change keymaps textobjects.move

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -90,26 +90,24 @@ local function setup_treesitter(config)
 					["<leader>A"] = "@parameter.inner",
 				},
 			},
-			textobjects = {
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["<M-n>"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["<M-N>"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["<M-m>"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["<M-M>"] = "@function.outer",
+					["[]"] = "@class.outer",
 				},
 			},
 		},
