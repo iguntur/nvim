dda45e6d3630846e7b8c45060afcf1b6fe911d3c
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-29 23:08:06 +0800

    fix(git): use vim keymap instead to fix keybinding

--- lua/plugins/git-integration.lua
+++ lua/plugins/git-integration.lua
@@ -23,29 +23,32 @@ M.setup = function(use)
 			-- },
 			-- current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
-
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
-
-				-- Navigation
-				map('n', ']c', function()
-					if vim.wo.diff then return ']c' end
-					vim.schedule(function() gs.next_hunk() end)
-					return '<Ignore>'
-				end, { expr = true })
-
-				map('n', '[c', function()
-					if vim.wo.diff then return '[c' end
-					vim.schedule(function() gs.prev_hunk() end)
-					return '<Ignore>'
-				end, { expr = true })
-			end,
+			-- on_attach = function(bufnr)
+			-- 	local gs = package.loaded.gitsigns
+			--
+			-- 	local function map(mode, l, r, opts)
+			-- 		opts = opts or {}
+			-- 		opts.buffer = bufnr
+			-- 		vim.keymap.set(mode, l, r, opts)
+			-- 	end
+			--
+			-- 	-- Navigation
+			-- 	map('n', ']c', function()
+			-- 		if vim.wo.diff then return ']c' end
+			-- 		vim.schedule(function() gs.next_hunk() end)
+			-- 		return '<Ignore>'
+			-- 	end, { expr = true })
+			--
+			-- 	map('n', '[c', function()
+			-- 		if vim.wo.diff then return '[c' end
+			-- 		vim.schedule(function() gs.prev_hunk() end)
+			-- 		return '<Ignore>'
+			-- 	end, { expr = true })
+			-- end,
 		})
+
+		vim.keymap.set("n", "]c", "<cmd>Gitsigns next_hunk<CR>")
+		vim.keymap.set("n", "[c", "<cmd>Gitsigns prev_hunk<CR>")
 	end)
 end
 
