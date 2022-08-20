1e26ee86dd63737ec56528c3fc7fc0a60d9fa94a
Guntur Poetra <poetra.guntur@gmail.com>
2022-08-21 01:41:30 +0800

    style(): 💄

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -141,10 +141,18 @@ local function open_files_command()
 end
 
 local function harpoon_keymap_setup()
-	vim.keymap.set("n", "zm", function() require("harpoon.mark").add_file() end)
-	vim.keymap.set("n", "zs", function() require("harpoon.ui").toggle_quick_menu() end)
-	vim.keymap.set("n", "zj", function() require("harpoon.ui").nav_next() end)
-	vim.keymap.set("n", "zk", function() require("harpoon.ui").nav_prev() end)
+	vim.keymap.set("n", "zm", function()
+		require("harpoon.mark").add_file()
+	end)
+	vim.keymap.set("n", "zs", function()
+		require("harpoon.ui").toggle_quick_menu()
+	end)
+	vim.keymap.set("n", "zj", function()
+		require("harpoon.ui").nav_next()
+	end)
+	vim.keymap.set("n", "zk", function()
+		require("harpoon.ui").nav_prev()
+	end)
 end
 
 local function telescope_setup()
