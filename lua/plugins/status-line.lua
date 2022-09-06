48739a36022ec568f55b47baa7507a45b00f5897
Guntur Poetra <poetra.guntur@gmail.com>
2022-09-07 01:14:13 +0800

    chore(plugins): show fullpath in the status line

--- lua/plugins/status-line.lua
+++ lua/plugins/status-line.lua
@@ -31,15 +31,62 @@ local function lualine_setup(lualine)
 	})
 end
 
+local assets = {
+	left_separator = "",
+	right_separator = "",
+	bar = "█",
+	mode_icon = "",
+	dir = "  ",
+	file = "  ",
+	lsp = {
+		server = "  ",
+		error = "  ",
+		warning = "  ",
+		info = "  ",
+		hint = "  ",
+	},
+	git = {
+		branch = "  ",
+		added = "  ",
+		changed = "  ",
+		removed = "  ",
+	},
+}
+
 local function feline_setup(feline)
 	local options = {}
-	local ok, components = pcall(require, "catppuccin.core.integrations.feline")
+	local ok, ctp_feline = pcall(require, "catppuccin.groups.integrations.feline")
 
-	if ok then
-		options.components = components
+	if not ok then
+		vim.notify("catppuccin feline integrations not found")
+		return
 	end
 
-	feline.setup(options)
+	local show_modified = false
+
+	--	ctp_feline.setup()
+	local components = ctp_feline.get()
+	local filebar = components.active[3][3]
+
+	filebar.provider = function()
+		local filename = vim.fn.expand("%:f") -- t | f
+		local extension = vim.fn.expand("%:e")
+		local icon = require("nvim-web-devicons").get_icon(filename, extension)
+
+		if icon == nil then
+			icon = assets.file
+		end
+
+		return (show_modified and "%m" or "") .. " " .. icon .. " " .. filename .. " "
+	end
+
+	components.active[3][3] = filebar
+
+	feline.setup({
+		components = components,
+	})
+
+	-- feline.winbar.setup()
 end
 
 M.setup = function(use)
