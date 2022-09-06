e9bceea2709b937ca6d1382b30119a1adb443022
Guntur Poetra <poetra.guntur@gmail.com>
2022-09-07 01:13:33 +0800

    chore(plugins): new setup

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -13,14 +13,25 @@ M.setup = function(use)
 	use("EdenEast/nightfox.nvim") -- nightfox, nordfox, dayfox, dawnfox and duskfox
 	use("folke/tokyonight.nvim") -- tokyonight
 	use("shaunsingh/moonlight.nvim") -- moonlight
-	-- use('Shatur/neovim-ayu') -- ayu-<dark,light,mirage>
+	use("Shatur/neovim-ayu") -- ayu-<dark,light,mirage>
 	-- use('glepnir/zephyr-nvim') -- zephyr
 	-- use('ray-x/aurora') -- aurora
 	use("RRethy/nvim-base16") -- base-16-<color-name>
 	use({ "catppuccin/nvim", as = "catppuccin" })
 
 	-- local colorscheme = "gruvbox"
-	local colorscheme = "catppuccin"
+	-- local colorscheme = "catppuccin"
+	local colorscheme = "base16-ayu-mirage"
+
+	SafeRequire("catppuccin", function(catppuccin)
+		vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+		catppuccin.setup({
+			-- integrations = {
+			--
+			-- },
+		})
+	end)
 
 	local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
