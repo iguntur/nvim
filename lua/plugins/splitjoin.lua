1485470af5088039768eb56316cb97d4ba1b5778
Guntur Poetra <poetra.guntur@gmail.com>
2022-10-02 13:19:01 +0700

    chore(plugins): add docs keymap

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -2,6 +2,9 @@ local M = {}
 
 function M.setup(use)
 	use("AndrewRadev/splitjoin.vim")
+
+	-- gS - to split a one-liner into multiple lines
+	-- gJ - (with the cursor on the first line of a block) to join a block into a single-line statement.
 end
 
 return M
