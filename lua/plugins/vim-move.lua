113fe0dba3b3192bb69764d7a487080c338e1618
Guntur Poetra <poetra.guntur@gmail.com>
2022-10-02 11:46:17 +0700

    fix vim move

--- lua/plugins/vim-move.lua
+++ lua/plugins/vim-move.lua
@@ -1,10 +1,21 @@
 local M = {}
 
+-- local function keymap(mode, lhs, rhs)
+-- 	vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+-- end
+
 M.setup = function(use)
-	use({ "matze/vim-move", tag = "v1.3" }) -- Move line
+	use({ "matze/vim-move" }) -- Move line
 
 	-- Move line
-	-- vim.g.move_key_modifier = 'C' -- Default 'A' (Alt)
+	vim.g.move_key_modifier_visualmode = "M"
+	vim.g.move_key_modifier = "M" -- Default 'A' (Alt)
+	-- vim.g.move_map_keys = 0
+
+	-- keymap("n", "<Esc><Left>", "<Plug>MoveCharLeft<Esc>")
+	-- keymap("n", "<Esc><Right>", "<Plug>MoveCharRight<Esc>")
+	-- keymap("v", "<C-M-h>", "<Plug>MoveCharLeft")
+	-- keymap("v", "<C-M-l>", "<Plug>MoveCharRight")
 end
 
 return M
