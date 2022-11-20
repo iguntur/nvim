c5802bd28b21863a0e6bf2a877617da2339e3dbb
Guntur Poetra <poetra.guntur@gmail.com>
2022-11-21 01:48:19 +0800

    fix(): fix deprecated LSP api

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,11 +2,7 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	group = vim.api.nvim_create_augroup("GT_GoFiletype", { clear = true }),
 	pattern = "*.go",
 	callback = function()
-		-- vim.lsp.buf.formatting_seq_sync()
-		-- vim.lsp.buf.formatting_sync(nil, 500)
 		vim.lsp.buf.format()
-		-- vim.lsp.format()
-
 		vim.cmd([[ write ]])
 	end,
 })
--- lua/plugins/lsp/handler.lua
+++ lua/plugins/lsp/handler.lua
@@ -179,7 +179,8 @@ M.setup = function()
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+-- M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
 		client.server_capabilities.document_formatting = false
