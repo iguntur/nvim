77dc8d44f76b1bc6fb5a87e16fc4b82d5c194104
Guntur Poetra <poetra.guntur@gmail.com>
2022-12-25 19:52:25 +0800

    adjust keymap

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -5,28 +5,7 @@ vim.g.maplocalleader = ","
 --------------------------------------------------------------------------------
 -- Map Helper Function
 --------------------------------------------------------------------------------
-local default_opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
-
-local function n_map(lhs, rhs, options)
-	keymap("n", lhs, rhs, options)
-end
-
-local function i_map(lhs, rhs, options)
-	keymap("i", lhs, rhs, options)
-end
-
-local function v_map(lhs, rhs, options)
-	keymap("v", lhs, rhs, options)
-end
-
-local function t_map(lhs, rhs, options)
-	keymap("t", lhs, rhs, options)
-end
-
-local function x_map(lhs, rhs, options)
-	keymap("x", lhs, rhs, options)
-end
+local keymap = vim.keymap.set
 
 --------------------------------------------------------------------------------
 -- Common Shortcut
@@ -40,165 +19,166 @@ end
 keymap("", "gf", ":edit <cfile><CR>", { silent = true })
 
 -- Reload (source nvim.config): F5
--- n_map("<F5>", ":source $HOME/.config/nvim/init.lua<CR><Esc>", { silent = true })
+-- keymap("n", "<F5>", ":source $HOME/.config/nvim/init.lua<CR><Esc>", { silent = true })
 
 -- Disable Arrow-keys
 for _, k in pairs({ "<Up>", "<Down>", "<Left>", "<Right>" }) do
-	n_map(k, "<Nop>", default_opts)
-	v_map(k, "<Nop>", default_opts)
+	keymap("n", k, "<Nop>")
+	keymap("v", k, "<Nop>")
 end
 
 -- Disable terminal clear scren (Cmd+k)
-keymap("", "<Cmd>k", "<Nop>", { silent = true })
+-- keymap("", "<Cmd>k", "<Nop>", { silent = true })
 keymap("", "<Cmd-k>", "<Nop>", { silent = true })
 
-n_map("q:", "<Nop>", default_opts) -- disable unexpected macro
+keymap("n", "q:", "<Nop>") -- disable unexpected macro
 
 -- Set space/del/backspace to Esc
--- n_map("<space>", "<Esc>", default_opts)
-n_map("<Del>", "<Esc>", default_opts)
-n_map("<BS>", "<Esc>", default_opts)
--- n_map('<C-space>', '<Esc>0', default_opts) -- Control+space
+-- keymap("n", "<space>", "<Esc>")
+keymap("n", "<Del>", "<Esc>")
+keymap("n", "<BS>", "<Esc>")
+-- keymap("n", '<C-space>', '<Esc>0') -- Control+space
 
 -- Clear the highlight search
-n_map("<Esc>", ":nohlsearch<CR><Esc>", default_opts)
+keymap("n", "<Esc>", ":nohlsearch<CR><Esc>")
 
 --------------------------------------------------------------------------------
 -- Text Editing
 --------------------------------------------------------------------------------
 -- Copy-Paste
-v_map("p", '"_dP', default_opts) -- keep the "yank" value
+keymap("v", "p", '"_dP') -- keep the "yank" value
 
 -- Pair Space
-i_map("<M-Space>", "<Esc>2a<space><Esc>i", default_opts)
-n_map("<M-Space>", "<Esc>2a<space><Esc>i", default_opts)
+-- keymap("i", "<M-Space>", "<Esc>2a<space><Esc>i")
+-- keymap("n", "<M-Space>", "<Esc>2a<space><Esc>i")
+keymap({ "n", "i" }, "<M-Space>", "<Esc>2a<space><Esc>i")
 
 --------------------------------------------------------------------------------
 -- Cursor movement
 --------------------------------------------------------------------------------
 -- Move cursor to the first character: (shift + h)
-n_map("H", "^<ESC>", default_opts)
+keymap("n", "H", "^<ESC>")
 
 -- Move cursor to the last character: (shift + l)
-n_map("L", "$<ESC>", default_opts)
+keymap("n", "L", "$<ESC>")
 
 -- Move cursor when insert mode: (control) + (h/j/k/l)
-i_map("<C-h>", "<Left>", default_opts)
-i_map("<C-j>", "<Down>", default_opts)
-i_map("<C-k>", "<Up>", default_opts)
-i_map("<C-l>", "<Right>", default_opts)
+keymap("i", "<C-h>", "<Left>")
+keymap("i", "<C-j>", "<Down>")
+keymap("i", "<C-k>", "<Up>")
+keymap("i", "<C-l>", "<Right>")
 
-n_map("<C-k>", "9kzz", default_opts) -- up 9 lines
-n_map("<C-j>", "9jzz", default_opts) -- down 9 lines
+keymap("n", "<C-k>", "9kzz") -- up 9 lines
+keymap("n", "<C-j>", "9jzz") -- down 9 lines
 
-v_map("<M-;>", "9k", default_opts)
-v_map("<M-'>", "9j", default_opts)
+keymap("v", "<M-;>", "9k") -- select in visual mode
+keymap("v", "<M-'>", "9j") -- select in visual mode
 --------------------------------------------------------------------------------
 -- Buffer and File
 --------------------------------------------------------------------------------
 -- Write or saving file: (⌥ or alt) + s
-v_map("<M-s>", "<ESC>:w<CR><ESC>", default_opts)
-n_map("<M-s>", ":w<CR><ESC>", default_opts)
-i_map("<M-s>", "<ESC>:w<CR><ESC>a", default_opts) -- allow writing in insert mode
+keymap("v", "<M-s>", "<ESC>:w<CR><ESC>")
+keymap("n", "<M-s>", ":w<CR><ESC>")
+keymap("i", "<M-s>", "<ESC>:w<CR><ESC>a") -- allow writing in insert mode
 
 -- Close current buffer: (⌥ or alt) + w
--- n_map('<M-w>', ':bw<ESC>', default_opts)
-n_map("<M-w>", ":BufferClose<CR>", default_opts)
-n_map("<M-W>", ":BufferClose!<CR>", default_opts)
+-- keymap("n", '<M-w>', ':bw<ESC>')
+keymap("n", "<M-w>", ":BufferClose<CR>")
+keymap("n", "<M-W>", ":BufferClose!<CR>")
 
 --------------------------------------------------------------------------------
 -- Navigate buffers
 --------------------------------------------------------------------------------
--- n_map('<TAB>', ':bnext<CR>', default_opts) -- tab
--- n_map('<S-TAB>', ':bprevious<CR>', default_opts) -- shift+tab
-n_map("<TAB>", ":bprevious<CR>", default_opts) -- tab
-n_map("<S-TAB>", ":bnext<CR>", default_opts) -- shift+tab
+-- keymap("n", '<TAB>', ':bnext<CR>') -- tab
+-- keymap("n", '<S-TAB>', ':bprevious<CR>') -- shift+tab
+keymap("n", "<TAB>", ":bprevious<CR>") -- tab
+keymap("n", "<S-TAB>", ":bnext<CR>") -- shift+tab
 
 --------------------------------------------------------------------------------
 -- Indentation
 --------------------------------------------------------------------------------
 -- indent to the right: Shift + >
-v_map(">", ">gv", default_opts)
-n_map(">", ">>_", default_opts)
+keymap("v", ">", ">gv")
+keymap("n", ">", ">>_")
 
 -- indent to the left: shift + <
-v_map("<", "<gv", default_opts)
-n_map("<", "<<_", default_opts)
+keymap("v", "<", "<gv")
+keymap("n", "<", "<<_")
 
 --------------------------------------------------------------------------------
 -- Insert Empty Line(s)
 --------------------------------------------------------------------------------
 -- Insert empty line on top cursor and up: Control+Alt+k
-n_map("<M-C-K>", '<S-o><ESC>"_cc<ESC>', default_opts)
+keymap("n", "<M-C-K>", '<S-o><ESC>"_cc<ESC>')
 
 -- Insert empty line on bottom cursor and down: Control+Alt+j
-n_map("<M-NL>", 'o<ESC>"_cc<ESC>', default_opts)
+keymap("n", "<M-NL>", 'o<ESC>"_cc<ESC>')
 
 --------------------------------------------------------------------------------
 -- Duplicate Line(s)
 --------------------------------------------------------------------------------
 -- Duplicate Line Up: Alt+Shift+k
-n_map("<M-K>", '<ESC>^<ESC>"0yy "0P<ESC>', default_opts)
-v_map("<M-K>", "<NOP>", default_opts)
+keymap("n", "<M-K>", '<ESC>^<ESC>"0yy "0P<ESC>')
+keymap("v", "<M-K>", "<NOP>")
 
 -- Duplicate Line Down: Alt+Shift+j
-n_map("<M-J>", '<ESC>^<ESC>"0yy "0p<ESC>', default_opts)
-v_map("<M-J>", "<NOP>", default_opts)
+keymap("n", "<M-J>", '<ESC>^<ESC>"0yy "0p<ESC>')
+keymap("v", "<M-J>", "<NOP>")
 
 --------------------------------------------------------------------------------
 -- Delete Line(s)
 --------------------------------------------------------------------------------
 -- Remove(Cut) active line: Alt+d
-n_map("<M-d>", '"_dd<ESC>', default_opts)
-v_map("<M-d>", '"_d<ESC>', default_opts)
+keymap("n", "<M-d>", '"_dd<ESC>')
+keymap("v", "<M-d>", '"_d<ESC>')
 
 --------------------------------------------------------------------------------
 -- Utilities
 --------------------------------------------------------------------------------
 -- Sort Lines Natural (Asc): space+s
--- v_map('<space>s', ':\'<,\'>!sort -f<CR><ESC>', default_opts)
+-- keymap("v", '<space>s', ':\'<,\'>!sort -f<CR><ESC>')
 
 --------------------------------------------------------------------------------
 -- Window
 --------------------------------------------------------------------------------
 -- Window Navigation
-n_map("<C-Left>", "<C-w>h", default_opts) -- move cursor to "left" window
-n_map("<C-Down>", "<C-w>j", default_opts) -- move cursor to "bottom" window
-n_map("<C-Up>", "<C-w>k", default_opts) -- move cursor to "top" window
-n_map("<C-Right>", "<C-w>l", default_opts) -- move cursor to "right" window
+keymap("n", "<C-Left>", "<C-w>h") -- move cursor to "left" window
+keymap("n", "<C-Down>", "<C-w>j") -- move cursor to "bottom" window
+keymap("n", "<C-Up>", "<C-w>k") -- move cursor to "top" window
+keymap("n", "<C-Right>", "<C-w>l") -- move cursor to "right" window
 
 --------------------------------------------------------------------------------
 -- Resize window (Ctrl-Shift-<Arrow_Keys>)
 --------------------------------------------------------------------------------
-n_map("<C-S-Up>", ":resize +1<CR>", default_opts) -- " Up
-n_map("<C-S-Down>", ":resize -1<CR>", default_opts) -- " Down
-n_map("<C-S-Left>", ":vertical resize -1<CR>", default_opts) -- " Left
-n_map("<C-S-Right>", ":vertical resize +1<CR>", default_opts) -- " Right
+keymap("n", "<C-S-Up>", ":resize +1<CR>") -- " Up
+keymap("n", "<C-S-Down>", ":resize -1<CR>") -- " Down
+keymap("n", "<C-S-Left>", ":vertical resize -1<CR>") -- " Left
+keymap("n", "<C-S-Right>", ":vertical resize +1<CR>") -- " Right
 
 --------------------------------------------------------------------------------
 -- Terminal
 --------------------------------------------------------------------------------
 -- Terminal navigation
-t_map("<C-h>", "<C-\\><C-N><C-w>h", default_opts)
-t_map("<C-j>", "<C-\\><C-N><C-w>j", default_opts)
-t_map("<C-k>", "<C-\\><C-N><C-w>k", default_opts)
-t_map("<C-l>", "<C-\\><C-N><C-w>l", default_opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h")
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j")
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k")
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l")
 
 --------------------------------------------------------------------------------
 -- Misc
 --------------------------------------------------------------------------------
 -- do not yank with x
-vim.keymap.set("n", "x", '"_x')
+keymap("n", "x", '"_x')
 
 -- Increment/decrement
-vim.keymap.set("n", "+", "<C-a>")
-vim.keymap.set("n", "-", "<C-x>")
+keymap("n", "+", "<C-a>")
+keymap("n", "-", "<C-x>")
 
 -- delete word from the end
-vim.keymap.set("n", "dw", 'viw"_d')
+keymap("n", "dw", 'viw"_d')
 
 -- select all
-vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+keymap("n", "<C-a>", "gg<S-v>G")
 
 --------------------------------------------------------------------------------
 -- ...
