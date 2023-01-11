7206bc95f25a77a0e34a18c2bcaba15239acc851
Guntur Poetra <poetra.guntur@gmail.com>
2023-01-11 21:26:39 +0800

    chore(): todo comment plugins

--- init.lua
+++ init.lua
@@ -339,6 +339,21 @@ return packer.startup(function(use)
 	-- })
 
 	--
+	-- todo comments
+	--
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+
+	--
 	-- ...
 	--
 end)
