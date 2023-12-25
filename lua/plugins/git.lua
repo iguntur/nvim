9418e221ab3c121052fec043251c790d63d09ecf
Guntur Poetra <poetra.guntur@gmail.com>
2023-12-26 02:43:02 +0800

    feat(git): git worktree

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,4 +5,42 @@ return {
     {
         "tpope/vim-fugitive",
     },
+
+    --
+    -- Git worktree
+    --
+    {
+        "ThePrimeagen/git-worktree.nvim",
+        dependencies = {
+            { "nvim-telescope/telescope.nvim" },
+        },
+        config = function()
+            require("telescope").load_extension("git_worktree")
+        end,
+        keys = {
+            {
+                "<leader>gwa",
+                function()
+                    local branch = vim.fn.input("Add: ")
+                    require("git-worktree").create_worktree(branch, branch, "origin")
+                end,
+                desc = "Create a new worktree",
+            },
+            {
+                "<leader>gws",
+                function()
+                    vim.cmd("silent !tmux neww gw-switch")
+                end,
+                desc = "Switch to a worktree branch",
+            },
+            {
+                "<leader>gwd",
+                function()
+                    local branch = vim.fn.input("Delete: ")
+                    require("git-worktree").delete_worktree(branch)
+                end,
+                desc = "Delete a worktree branch",
+            },
+        },
+    },
 }
