1edaadcc3ca103e870fe1ba3bf9f65587aab1804
Guntur Poetra <poetra.guntur@gmail.com>
2024-01-04 13:04:10 +0800

    fix(telescope): riprep arguments

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 local util = require("util")
+local LazyUtil = require("lazyvim.util")
 
 local function find_cwd_files()
     local opts = {
@@ -40,6 +41,30 @@ local function find_project_root_files()
     require("telescope.builtin").find_files(opts)
 end
 
+local function live_grep_project()
+    local opts = {
+        prompt_title = "~ LIVE GREP PROJECT FILES ~",
+        cwd = util.find_dir_up({ ".git", "Makefile", "go.work", ".nvmrc", "go.mod", "package.json" }),
+        vimgrep_arguments = {
+            "rg",
+            "--follow",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--ignore-file",
+            vim.env.HOME .. "/.rgignore",
+            "--glob",
+            "!**/.git/*",
+        },
+    }
+
+    require("telescope.builtin").live_grep(opts)
+end
+
 return {
     --
     -- telescope default options
@@ -48,25 +73,31 @@ return {
         "nvim-telescope/telescope.nvim",
         optional = true,
         opts = function(_, opts)
-            local config = {
-                vimgrep_arguments = {
-                    "rg",
-                    "--follow",
-                    "--color=never",
-                    "--no-heading",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                    "--no-ignore",
-                    "--hidden",
-                    "--ignore-file",
-                    vim.env.HOME .. "/.rgignore",
-                    "--sort",
-                    "path",
-                    -- "-u", -- alias for --unrestricted
-                    -- "--trim" -- add this value
-                },
+            local telescopeConfig = require("telescope.config")
+
+            -- Clone the default Telescope configuration
+            local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+            local argvs = {
+                "--follow",
+                "--hidden",
+                "--ignore-file",
+                vim.env.HOME .. "/.rgignore",
+                "--glob",
+                "!**/.git/*",
+                -- "-u", -- alias for --unrestricted
+                -- "--trim" -- add this value
+            }
+
+            for _, argv in ipairs(argvs) do
+                table.insert(vimgrep_arguments, argv)
+            end
+
+            --
+            -- default config
+            --
+            local defaults = {
+                -- path_display = { "smart" }, -- smart,truncate,shorten. default: truncate
+                vimgrep_arguments = vimgrep_arguments,
                 layout_strategy = "bottom_pane", -- vertical, horizontal, flex, bottom_pane
                 layout_config = {
                     prompt_position = "top",
@@ -74,9 +105,32 @@ return {
                 sorting_strategy = "ascending",
             }
 
-            opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, config)
+            --
+            -- extensions
+            --
+            local extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
+            }
+
+            opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, defaults)
+            opts.extensions = vim.tbl_deep_extend("force", opts.extensions or {}, extensions)
         end,
         keys = {
+            {
+                "<leader>/",
+                function()
+                    live_grep_project()
+                end,
+                remap = true,
+                silent = true,
+                desc = "Telescope live grep",
+            },
             {
                 "<C-p>",
                 function()
@@ -105,6 +159,11 @@ return {
                 end,
                 desc = "Find files (project directory)",
             },
+            {
+                "<leader>fg",
+                "<CMD>Telescope git_status<CR>",
+                desc = "Telescope git status",
+            },
         },
     },
 
@@ -118,7 +177,9 @@ return {
             {
                 "nvim-telescope/telescope-file-browser.nvim",
                 config = function()
-                    require("telescope").load_extension("file_browser") -- load this after fzf
+                    LazyUtil.on_load("telescope.nvim", function()
+                        require("telescope").load_extension("file_browser") -- load this after fzf
+                    end)
                 end,
             },
         },
@@ -128,11 +189,6 @@ return {
                 "<cmd>Telescope file_browser path=%:p:h<CR>",
                 remap = true,
                 desc = "Telescope file browser",
-            },
-            {
-                "<leader>fg",
-                "<CMD>Telescope git_status<CR>",
-                desc = "Telescope git status",
             },
         },
     },
