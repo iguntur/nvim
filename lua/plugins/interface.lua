b72480ef4225c0d8100107ad7a5db52c27e17c14
Guntur Poetra <poetra.guntur@gmail.com>
2024-02-08 06:35:07 +0800

    plugin: enable winbar

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -130,18 +130,21 @@ return {
         end,
     },
 
-    -- {
-    --     "utilyre/barbecue.nvim",
-    --     name = "barbecue",
-    --     version = "*",
-    --     dependencies = {
-    --         "SmiteshP/nvim-navic",
-    --         "nvim-tree/nvim-web-devicons", -- optional dependency
-    --     },
-    --     opts = {
-    --         -- configurations go here
-    --     },
-    -- },
+    --
+    -- winbar feature
+    --
+    {
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        opts = {
+            -- configurations go here
+        },
+    },
 
     {
         "neovim/nvim-lspconfig",
