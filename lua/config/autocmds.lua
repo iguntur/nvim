90c0597200b34e3b33353a40204b50d05d053738
Guntur Poetra <poetra.guntur@gmail.com>
2024-02-07 22:40:50 +0800

    config: autocmd group

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,16 +1,25 @@
+local function augroup(name)
+    return vim.api.nvim_create_augroup("the_guntur_" .. name, { clear = true })
+end
+
 vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    group = augroup("nopaste"),
     pattern = { "*" },
     command = "set nopaste",
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = {
-        "json",
-        "jsonc",
-        "markdown",
-    },
+    group = augroup("conceal"),
+    pattern = { "json", "jsonc", "markdown" },
+    callback = function()
+        vim.wo.conceallevel = 0 -- normal -- normal
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = augroup("spell"),
+    pattern = { "json", "jsonc", "markdown" },
     callback = function()
         vim.wo.spell = false
-        vim.wo.conceallevel = 0 -- normal -- normal
     end,
 })
