7b0ebb4a6f01232655f818da1acd8cf4e2c03dce
Guntur Poetra <poetra.guntur@gmail.com>
2024-02-09 08:35:57 +0800

    plugin: LSP inlay hints for lua and typescript/javascript

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,12 +5,66 @@ return {
     {
         "neovim/nvim-lspconfig",
         event = "LazyFile",
+        dependencies = {
+            { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+            { "folke/neodev.nvim", opts = {} },
+            { "mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+        },
         opts = {
             -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
             -- Be aware that you also will need to properly configure your LSP server to
             -- provide the inlay hints.
             inlay_hints = {
                 enabled = true,
+            },
+            servers = {
+                lua_ls = {
+                    -- mason = false, -- set to false if you don't want this server to be installed with mason
+                    -- Use this to add any additional keymaps
+                    -- for specific lsp servers
+                    ---@type LazyKeysSpec[]
+                    -- keys = {},
+                    settings = {
+                        Lua = {
+                            hint = { enable = true },
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                        },
+                    },
+                },
+                tsserver = {
+                    settings = {
+                        typescript = {
+                            inlayHints = {
+                                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+                                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                                includeInlayVariableTypeHints = true,
+                                includeInlayFunctionParameterTypeHints = true,
+                                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                                includeInlayPropertyDeclarationTypeHints = true,
+                                includeInlayFunctionLikeReturnTypeHints = true,
+                                includeInlayEnumMemberValueHints = true,
+                            },
+                        },
+                        javascript = {
+                            inlayHints = {
+                                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+                                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                                includeInlayVariableTypeHints = true,
+                                includeInlayFunctionParameterTypeHints = true,
+                                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                                includeInlayPropertyDeclarationTypeHints = true,
+                                includeInlayFunctionLikeReturnTypeHints = true,
+                                includeInlayEnumMemberValueHints = true,
+                            },
+                        },
+                    },
+                },
             },
         },
     },
