cd1f0e192f1d9d9522d88dcd50f5502102c82762
Guntur Poetra <poetra.guntur@gmail.com>
2024-05-17 00:58:20 +0800

    config: filter ft and buftype when auto-resize window

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,3 +23,40 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
         vim.wo.spell = false
     end,
 })
+
+-- Focus window
+local ignore_filetypes = {
+    "neo-tree",
+    "NvimTree",
+}
+local ignore_buftypes = {
+    "nofile",
+    "prompt",
+    "popup",
+}
+
+local focus_group = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+
+vim.api.nvim_create_autocmd("WinEnter", {
+    group = focus_group,
+    callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
+        end
+    end,
+    desc = "Disable focus autoresize for BufType",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = focus_group,
+    callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+        else
+            vim.b.focus_disable = false
+        end
+    end,
+    desc = "Disable focus autoresize for FileType",
+})
