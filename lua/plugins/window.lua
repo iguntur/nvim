f75fe1212d5630e605b7c4cce10337e86868230f
Guntur Poetra <poetra.guntur@gmail.com>
2024-05-16 16:39:59 +0800

    plugin: add focusing window options

--- lua/plugins/window.lua
+++ lua/plugins/window.lua
@@ -1,7 +1,26 @@
 return {
+    --
+    -- Auto-Focusing and Auto-Resizing Splits/Windows
+    --
     {
         "nvim-focus/focus.nvim",
         version = "*",
-        opts = {},
+        opts = {
+            ui = {
+                -- number = false, -- Display line numbers in the focussed window only
+                -- relativenumber = false, -- Display relative line numbers in the focussed window only
+                -- hybridnumber = true, -- Display hybrid line numbers in the focussed window only
+                -- absolutenumber_unfocussed = true, -- Preserve absolute numbers in the unfocussed windows
+
+                -- cursorline = true, -- Display a cursorline in the focussed window only
+                -- cursorcolumn = true, -- Display cursorcolumn in the focussed window only
+                -- colorcolumn = {
+                --     enable = false, -- Display colorcolumn in the foccused window only
+                --     list = "+1", -- Set the comma-saperated list for the colorcolumn
+                -- },
+                -- signcolumn = true, -- Display signcolumn in the focussed window only
+                -- winhighlight = true, -- Auto highlighting for focussed/unfocussed windows
+            },
+        },
     },
 }
