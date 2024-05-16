c541e958c5d20cf6a9ea205b5993aaf768a55d79
Guntur Poetra <poetra.guntur@gmail.com>
2024-05-16 16:39:36 +0800

    plugin: add noice with options

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -32,12 +32,29 @@ return {
         },
     },
 
+    --
+    -- noice: UI commandline, messages, etc...
+    --
     {
-        "craftzdog/solarized-osaka.nvim",
-        lazy = true,
-        priority = 1000,
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
         opts = {
-            transparent = true,
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = true, -- enables an input dialog for inc-rename.nvim
+                -- lsp_doc_border = false, -- add a border to hover docs and signature help
+            }, --
         },
     },
 
