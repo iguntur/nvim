42a61eac0be75cdffc57ba8c07a1f200be9d0c40
Guntur Poetra <poetra.guntur@gmail.com>
2024-05-19 22:27:39 +0800

    plugin: add oil - create file(s)

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -1,5 +1,23 @@
 return {
     --
+    -- add file/directory like buffer
+    --
+    {
+        "stevearc/oil.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("oil").setup()
+
+            vim.keymap.set("n", "<space>;", "<CMD>Oil<CR>", {
+                silent = true,
+                desc = "Open parent directory",
+            })
+        end,
+    },
+
+    --
     -- neo-tree: file explorer
     --
     {
