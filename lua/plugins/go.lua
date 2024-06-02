f41fed92e9b69049576609733b8c3b7cd467b86a
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-03 02:38:23 +0800

    plugin(lsp): lazy event

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -15,10 +15,10 @@ return {
         "ray-x/go.nvim",
         dependencies = { -- optional packages
             "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
             "nvim-treesitter/nvim-treesitter",
             {
                 "neovim/nvim-lspconfig",
+                event = "LazyFile",
                 opts = {
                     document_highlight = {
                         enabled = false,
