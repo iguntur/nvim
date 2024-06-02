1f8ac48ba370f2a5df497c18c9a91c2d07d1b18c
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-03 02:37:50 +0800

    plugin(lsp): remove neodev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
         event = "LazyFile",
         dependencies = {
             { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-            -- { "folke/neodev.nvim", opts = {} },
             { "mason.nvim" },
             { "williamboman/mason-lspconfig.nvim" },
         },
