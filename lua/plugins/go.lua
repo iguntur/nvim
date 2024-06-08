76790a066395e418a890326e019770f8f7d1f9b7
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-08 15:36:35 +0800

    adjust go + lsp

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,5 +1,29 @@
 return {
     {
+        "neovim/nvim-lspconfig",
+        event = "LazyFile",
+        opts = {
+            document_highlight = {
+                enabled = false,
+            },
+
+            servers = {
+                gopls = {
+                    settings = {
+                        gopls = {
+                            buildFlags = { "-tags", "wireinject" },
+                        },
+                    },
+
+                    -- keys = {
+                    --     { "<leader>cr", "<cmd>GoRename<CR>", desc = "Rename References" },
+                    -- },
+                },
+            },
+        },
+    },
+
+    {
         "olexsmir/gopher.nvim",
         dependencies = { -- dependencies
             "nvim-lua/plenary.nvim",
@@ -9,46 +33,33 @@ return {
     },
 
     --
-    -- go
+    -- json to struct
     --
     {
-        "ray-x/go.nvim",
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "nvim-treesitter/nvim-treesitter",
-            {
-                "neovim/nvim-lspconfig",
-                event = "LazyFile",
-                opts = {
-                    document_highlight = {
-                        enabled = false,
-                    },
-
-                    servers = {
-                        gopls = {
-                            settings = {
-                                gopls = {
-                                    buildFlags = { "-tags", "wireinject" },
-                                },
-                            },
-
-                            keys = {
-                                { "<leader>cr", "<cmd>GoRename<CR>", desc = "Rename References" },
-                            },
-                        },
-                    },
-                },
-            },
-        },
-        config = function()
-            require("go").setup()
-        end,
-        event = { "CmdlineEnter" },
-        ft = {
-            "go",
-            "gomod",
-            -- "gowork",
-        },
-        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+        "triarius/json2struct.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        ft = { "go" },
+        opts = {},
     },
+
+    --
+    -- go
+    --
+    -- {
+    --     "ray-x/go.nvim",
+    --     dependencies = { -- optional packages
+    --         "ray-x/guihua.lua",
+    --         "nvim-treesitter/nvim-treesitter",
+    --     },
+    --     config = function()
+    --         require("go").setup()
+    --     end,
+    --     event = { "CmdlineEnter" },
+    --     ft = {
+    --         "go",
+    --         "gomod",
+    --         -- "gowork",
+    --     },
+    --     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    -- },
 }
