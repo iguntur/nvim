822b7674e5604c97f37b6e5e5db379f520955d58
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-29 05:50:11 +0800

    feat: cmp plugins cmdline

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        event = "LazyFile",
+        dependencies = {
+            { "hrsh7th/cmp-cmdline" },
+        },
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" },
+                        },
+                    },
+                }),
+            })
+        end,
+    },
+}
