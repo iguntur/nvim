fd9df343f0f264f7dcfcbea8ccccc1bfec872101
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-30 04:06:55 +0800

    feat: add plugins cmp emoji

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,20 @@
 return {
     {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-emoji",
+        },
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            table.insert(opts.sources, { name = "emoji" })
+        end,
+    },
+
+    --
+    -- cmp cmdline
+    --
+    {
+        "hrsh7th/nvim-cmp",
         version = false,
         event = "LazyFile",
         dependencies = {
