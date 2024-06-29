0d61f2c4889c29090f5a4ee286c6a4d3a283cfb1
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-30 02:26:11 +0800

    feat: import plugins extran lang.git

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
         -- { import = "lazyvim.plugins.extras.linting.eslint" },
 
         -- lang
+        { import = "lazyvim.plugins.extras.lang.git" },
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.go" },
         { import = "lazyvim.plugins.extras.lang.sql" },
