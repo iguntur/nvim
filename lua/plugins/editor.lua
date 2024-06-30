6298365ea5fa85d533aa25761eb8b62661e46071
Guntur Poetra <poetra.guntur@gmail.com>
2024-06-30 23:36:26 +0800

    feat: add plugins twilight

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -319,4 +319,23 @@ return {
         "Darazaki/indent-o-matic",
         opts = {},
     },
+
+    -- Lua
+    {
+        "folke/twilight.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<space>tw",
+                ":Twilight<CR>",
+                noremap = true,
+                silent = true,
+                desc = "Twilight toggle",
+            },
+        },
+    },
 }
