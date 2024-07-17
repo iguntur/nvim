fd5f2f7145ac785390b0af85ef8ad58907a903c1
Guntur Poetra <poetra.guntur@gmail.com>
2024-07-18 01:56:53 +0800

    feat: foldthemore

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -367,4 +367,57 @@ return {
             vim.keymap.set("n", "<M-l>", ":MoveHChar(1)<CR>", opts)
         end,
     },
+
+    --
+    -- fold
+    --
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        config = function()
+            vim.o.foldcolumn = "1" -- '0' is not bad
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            local ufo = require("ufo")
+
+            ufo.setup({
+                open_fold_hl_timeout = 150,
+                close_fold_kinds_for_ft = {
+                    -- default = { "imports", "comment" },
+                    -- json = { "array" },
+                    -- c = { "comment", "region" },
+                },
+                provider_selector = function()
+                    return {
+                        "lsp",
+                        "indent",
+                        -- "treesitter",
+                    }
+                end,
+            })
+
+            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+            vim.keymap.set("n", "zR", ufo.openAllFolds, {
+                silent = true,
+                noremap = true,
+                desc = "Open all folds...",
+            })
+            vim.keymap.set("n", "zM", ufo.closeAllFolds, {
+                silent = true,
+                noremap = true,
+                desc = "Close all folds...",
+            })
+            vim.keymap.set("n", "zK", function()
+                local winid = ufo.peekFoldedLinesUnderCursor()
+
+                if not winid then
+                    vim.lsp.buf.hover()
+                end
+            end, { silent = true, noremap = true, desc = "Peek fold" })
+        end,
+    },
 }
