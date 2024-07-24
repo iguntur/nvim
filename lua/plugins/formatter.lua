4487e4dfc3869617c517c65a1cc703280aff4e77
Guntur Poetra <poetra.guntur@gmail.com>
2024-07-24 21:57:02 +0800

    feat: templ formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -7,13 +7,13 @@ return {
                 formatters_by_ft = {
                     -- Conform will run multiple formatters sequentially
                     python = { "isort", "black" },
-                    -- Use a sub-list to run only the first available formatter
                     go = {
                         "gofumpt",
                         "goimports-reviser",
                         "golines",
                     },
                     svelte = { "prettier" },
+                    templ = { "templ" },
                 },
 
                 formatters = {
